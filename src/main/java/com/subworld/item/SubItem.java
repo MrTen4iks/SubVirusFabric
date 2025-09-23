b28a311794d88c8d2a5df330cab10a798b2ff2c2
MrTen4iks <MrTen4iks@yandex.ru>
2025-09-23 19:58:57 +0300

    1.1

---
+++ src/main/java/com/subworld/item/SubItem.java
@@ -0,0 +1,44 @@
+package com.subworld.item;
+
+import com.subworld.Subteam;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class SubItem {
+    public static final Item INFECTEDS_RAW = register("infecteds_raw", Item::new, new Item.Settings());
+    public static final Item INFECTED_INGOT = register("infected_ingot", Item::new, new Item.Settings());
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Subteam.MOD_ID, name));
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void registerModItems() {
+        Subteam.LOGGER.debug("Регестрация предметов для" + Subteam.MOD_ID);
+
+
+    }
+
+
+
+}
