65ffc11236450f353d1f1709c378d132999b0e07
Myron6767 <myronnovi@gmail.com>
2026-01-04 20:22:19 +0500

    Я лоханулся

--- src/main/java/com/subworld/subvirus/registry/SubItemGroups.java
+++ src/main/java/com/subworld/subvirus/registry/SubItemGroups.java
@@ -16,20 +16,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class SubItemGroups {
-    /*    public static final RegistryKey<ItemGroup> SUB_CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(SubVirus.MOD_ID, "sub_group"));
-    public static final ItemGroup SUB_CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
-            .icon(() -> new ItemStack(SubItems.INFECTED_INGOT))
-            .displayName(Text.translatable(SubVirus.MOD_ID+".group_trans"))
-            .build();
-    private static Collection<Item> items = new ArrayList<>();
-    private static Collection<ItemStack> stacks = new ArrayList<>();
-    public static void addItemToTab(Item i) {
-        items.add(i);
-    }
 
-    public static void addItemToTab(ItemStack s) {
-        stacks.add(s);
-    }*/
 
     public static final AbstractTab SUB_TAB = new AbstractTab(
             RegistryKey.of(
@@ -40,25 +27,14 @@ public class SubItemGroups {
                     .icon(() -> new ItemStack(SubItems.INFECTED_INGOT))
                     .displayName(Text.translatable(SubVirus.MOD_ID + ".group_trans"))
                     .build()
-    ) {};
+    );
 
     public static void registerModTabs(){
         System.out.println("Registering item groups for " + SubVirus.MOD_ID);
-       /* Registry.register(Registries.ITEM_GROUP, SUB_CUSTOM_ITEM_GROUP_KEY, SUB_CUSTOM_ITEM_GROUP);
-        ItemGroupEvents.modifyEntriesEvent(SubItemGroups.SUB_CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
-            for (Item i : items) {
-                System.out.println("add item to tab "+ i.getName());
-                itemGroup.add(i);
-            }
-            for (ItemStack i : stacks) {
-                System.out.println("add item to tab "+ i.getName());
-                itemGroup.add(i);
-            }
-        });*/
         SUB_TAB.register();
     }
 
-    public static abstract class AbstractTab {
+    public static class AbstractTab {
         protected final RegistryKey<ItemGroup> key;
         protected final ItemGroup group;
 
